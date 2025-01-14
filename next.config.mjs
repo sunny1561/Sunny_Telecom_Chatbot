7e31108eecab06c6014443fad74f587f5cf094f9
sunny1561 <106393457+sunny1561@users.noreply.github.com>
2025-01-14 22:07:57 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     // reactStrictMode: true,
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'img.freepik.com',
+//       },
+//     ],
+//   },
+// };
+
 const nextConfig = {
-    // reactStrictMode: true,
+  reactStrictMode: true, // Uncomment if you want React's strict mode.
   images: {
     remotePatterns: [
       {
@@ -8,6 +20,15 @@ const nextConfig = {
         hostname: 'img.freepik.com',
       },
     ],
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Exclude `onnxruntime-node` from being bundled by Webpack.
+      config.externals.push({
+        'onnxruntime-node': 'commonjs onnxruntime-node',
+      });
+    }
+    return config;
   },
 };
 
