78c92aee14e9f26dbd3014b9b4054a5ad71c5e1c
sunny1561 <106393457+sunny1561@users.noreply.github.com>
2025-01-14 17:28:46 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,15 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//     // reactStrictMode: true,
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: 'https',
-//         hostname: 'img.freepik.com',
-//       },
-//     ],
-//   },
-// };
-
-// export default nextConfig;
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config) => {
-        config.module.rules.push({
-            test: /\.node$/,
-            use: 'node-loader',
-        });
-        return config;
-    },
+    // reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.freepik.com',
+      },
+    ],
+  },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
+
